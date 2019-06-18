70241b3501b4af6a6aa867bdf2b5c9ee197a9373
jaishreekori <51723304+jaishreekori@users.noreply.github.com>
2019-06-19 02:13:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,52 +1,67 @@
 import React, { PureComponent } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, Platform } from 'react-native';
 import { RNCamera } from 'react-native-camera';
-import Img from './android/src/images'
 
 export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      img: ''
+    }
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera
-          ref={ref => {
-            this.camera = ref;
-          }}
-          style={styles.preview}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
+    if (this.state.isClicked) {
+      return <Image style={styles.images} source={{ uri: this.state.img }} ></Image>
+    } else {
+      return (
+        <View style={styles.container}>
+          <RNCamera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: 'Permission to use audio recording',
+              message: 'We need your permission to use your audio',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={({ barcodes }) => {
+              console.log(barcodes);
+            }}
 
-        />
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} 
-          style={styles.capture}
-          >
-            <Text style={{ fontSize: 14 }}> SNAP </Text>
-          </TouchableOpacity>
+          />
+          <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+            <TouchableOpacity onPress={this.takePicture.bind(this)}
+              style={styles.capture}
+            >
+              <Text style={{ fontSize: 14 }}> SNAP </Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-    );
+      );
+    }
+
   }
 
-  takePicture = async() => {
+  takePicture = async () => {
     if (this.camera) {
       const options = { quality: 0.5, base64: true };
       const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
+      this.setState({
+        img: data.uri,
+        isClicked: true
+      });
+      console.log(this.state.img);
     }
   };
 }
@@ -71,6 +86,14 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     margin: 20,
   },
+  images: {
+    flex: 0,
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginTop: 180,
+    paddingTop: (Platform.OS === 'android') ? 20 : 0
+  }
 });
-
-// AppRegistry.registerComponent('ExampleApp', () => ExampleApp);
